9445504aff59bbeddb6c17bf99934c48ce036b1a
gowshikan <raworldit@gmail.com>
2017-07-26 13:44:11 +0530

    Create eveeeeeeeeeeeeeeeeeeeeeen.cpp

---
+++ eveeeeeeeeeeeeeeeeeeeeeen.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int d;
+ int g;
+ 
+ 
+ cout<<"please enter num"<<endl;
+cin>>d;
+cout<<"enter num pleaseeeeeeeeeee program by programology site abdullah moeen"<<endl;
+cin>>g;
+while(d<g){
+ 
+ if(d%2==0)
+ {
+ cout<<"even no. ="<<d<<endl;
+ 
+ }
+ d=d+1;
+}
+}
